aac4b51a2dfbbb817afff7e494f252cf697e4c5a
Yev Kanivets <yevhenii.kanivets@gmail.com>
2021-12-01 15:40:27 +0200

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.map { it.toInt() }
+        .let { array ->
+            array.drop(1).foldIndexed(0) { index, acc, value ->
+                if (array[index] < value) acc + 1 else acc
+            }
+        }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = input.map { it.toInt() }
+        .let { array ->
+            array.drop(3).foldIndexed(0) { index, acc, value ->
+                val leftTriple = array[index] + array[index + 1] + array[index + 2]
+                val rightTriple = array[index + 1] + array[index + 2] + value
+                if (leftTriple < rightTriple) acc + 1 else acc
+            }
+        }
 
     val input = readInput("Day01")
     println(part1(input))
