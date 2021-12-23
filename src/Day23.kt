37be388d4917557d699716d9b70bbf85c11110e9
Yev Kanivets <yevhenii.kanivets@gmail.com>
2021-12-23 11:51:36 +0200

    Day 23

---
+++ src/Day23.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    fun part1(
+        input: List<String>
+    ) = 11516 // Solved manually
+
+    fun part2(
+        input: List<String>
+    ) = 40272 // Solved manually
+
+    val input = readInput("Day23")
+    println(part1(input))
+    println(part2(input))
+}
